033552f60e331f6abbdf0498085c298d78662042
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-19 15:27:43 +0200

    feat: add flag merge functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 const gameboard = document.getElementById('gameboard');
 
-let nextPosition = 2;
+let currentFlagId = 0;
 
 const flags = {
     "femaleFlag": {
@@ -16,9 +16,13 @@ const flags = {
         parents: [],
     },
     "gayFlag": {
+        position: 2,
         name: 'Gay Pride Flag',
         backgroundColor: 'red',
-        parents: {'a': 'Male Flag', 'b': 'Male Flag'},
+        parents: {
+            'a': 'Male Flag',
+            'b': 'Male Flag'
+        },
     },
 };
 
@@ -39,8 +43,32 @@ function createAndAppendDraggable(element) {
     const draggableDiv = document.createElement('div');
     draggableDiv.classList.add('draggable');
     draggableDiv.style.backgroundColor = element.backgroundColor;
-    draggableDiv.style.left = '10px'
+    draggableDiv.style.left = '10px';
     draggableDiv.style.top = 10 + element.position * 100 + 10 * element.position + 'px'
+    draggableDiv.textContent = element.name;
+
+    gameboard.appendChild(draggableDiv);
+    makeDraggableAndClone(draggableDiv, element);
+}
+
+function createAndAppendDraggable(element) {
+    const draggableDiv = document.createElement('div');
+    draggableDiv.classList.add('draggable');
+    draggableDiv.style.backgroundColor = element.backgroundColor;
+    draggableDiv.style.left = '10px';
+    draggableDiv.style.top = 10 + element.position * 100 + 10 * element.position + 'px';
+    draggableDiv.textContent = element.name;
+
+    gameboard.appendChild(draggableDiv);
+    makeDraggableAndClone(draggableDiv, element);
+}
+
+function createAndAppendDraggableWithPosition(element, position) {
+    const draggableDiv = document.createElement('div');
+    draggableDiv.classList.add('draggable');
+    draggableDiv.style.backgroundColor = element.backgroundColor;
+    draggableDiv.style.left = position.x + 'px';
+    draggableDiv.style.top = position.y + 'px';
     draggableDiv.textContent = element.name;
 
     gameboard.appendChild(draggableDiv);
@@ -51,7 +79,7 @@ function makeDraggableAndClone(draggable, jsObject) {
     // Initialize draggabilly and attach event handlers
     $(draggable).draggabilly({
         containment: true,
-    }).on('dragStart', function(event, pointer) {
+    }).on('dragStart', function (event, pointer) {
         // Bind this to dragStartHandler
         dragStartHandler.call(this, event, pointer, jsObject);
     }).on('dragEnd', dragEndHandler);
@@ -63,6 +91,8 @@ function makeDraggableAndClone(draggable, jsObject) {
 
 function dragStartHandler(event, pointer, jsObject) {
     const draggie = $(this).data('draggabilly');
+    draggie.metadata.id = currentFlagId;
+    currentFlagId++;
     draggedAwayFlags.push($(this));
     const position = draggie.position;
 
@@ -74,7 +104,7 @@ function dragStartHandler(event, pointer, jsObject) {
     const clonedObject = Object.assign({}, jsObject)
 
     gameboard.appendChild(clone);
-    makeDraggableAndClone(clone, jsObject);
+    makeDraggableAndClone(clone, clonedObject);
 }
 
 function dragEndHandler(event, pointer) {
@@ -85,23 +115,62 @@ function dragEndHandler(event, pointer) {
 
 
     draggedAwayFlags.forEach(flag => {
-        console.log(flag);
         const draggieY = flag.data('draggabilly');
+        console.log("DATA: ::::::::: " + draggieY);
         position = draggieY.position;
         console.log("position" + position.x);
         // check if the mouse cursor is on the flag
-        if (position.x <= pointer.pageX && pointer.pageX <= position.x + 100 &&
-            position.y <= pointer.pageY && pointer.pageY <= position.y + 100) {
+        const flagId = draggieY.metadata.id;
+        const draggedFlagId = draggieX.metadata.id;
+
+        console.log("FlagId: " + flagId + ", DraggedFlagId: " + draggedFlagId);
+
+        if (flagId === draggedFlagId) {
+            console.log("SAME ID");
+            return;
+        }
+
+        const adjustedPositionX = gameboard.offsetLeft + position.x;
+        const adjustedPositionY = gameboard.offsetTop + position.y;
+
+        if (adjustedPositionX <= pointer.pageX && pointer.pageX <= adjustedPositionX + 100 &&
+            adjustedPositionY <= pointer.pageY && pointer.pageY <= adjustedPositionY + 100) {
+            console.log("hi");
             // Mouse cursor is on the flag
             const nameParentY = draggieY.metadata.name;
-            print("Names. NameX: " + nameParentX + ", NameY: " + nameParentY);
+            console.log("Names. NameX: " + nameParentX + ", NameY: " + nameParentY);
+
+
+            for (const [key, value] of Object.entries(flags)) {
+                if (value.parents.a === nameParentX && value.parents.b === nameParentY ||
+                    value.parents.a === nameParentY && value.parents.b === nameParentX) {
+                    const newFlag = flags[key];
+
+                    createAndAppendDraggableWithPosition(newFlag, {
+                        x: position.x,
+                        y: position.y
+                    });
+                    createAndAppendDraggable(newFlag);
+
+                    console.log("make new flag");
+                    console.log(flag);
+                    flag.remove();
+                    $(this).remove();
+                    // I need to remove the flag from the draggedAwayFlags array
+                    // The thing is it needs to be a the specific flag
+                    draggedAwayFlags.splice(draggedAwayFlags.indexOf(flag), 1);
+                    draggedAwayFlags.splice(draggedAwayFlags.indexOf(this), 1);
+
+
+                }
+            }
         }
     });
 
-    flags.gayFlag.position = nextPosition;
-    nextPosition++;
-    discoveredFlags.push(flags.gayFlag);
-    createAndAppendDraggable(flags.gayFlag);
+    // flags.gayFlag.position = nextPosition;
+    // nextPosition++;
+    // discoveredFlags.push(flags.gayFlag);
+    // createAndAppendDraggable(flags.gayFlag);
 }
 
 // Initial call for existing draggable elements
