ee968adacc20b865e2199fec081bd18b7bf0841e
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-20 13:44:31 +0200

    refac: overhaul file to be object oriented
    also flags wont duplicate anymore when they are already dragged

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,165 @@
 const gameboard = document.getElementById('gameboard');
 
 let currentFlagId = 0;
+let flags = [];
+let domFlags = [];
 
 class Flag {
-    constructor(name, backgroundColor, position, parents) {
+    name;
+    backgroundColor; // This will be changed to image
+    parents;
+    id;
+    position = null;
+    discovered = false;
+    constructor(name, backgroundColor, parents, id) {
         this.name = name;
         this.backgroundColor = backgroundColor;
-        this.position = position;
         this.parents = parents;
-        this.draggies = [];
-        this.id = currentFlagId;
-        currentFlagId++;
+        this.id = id;
+    }
+
+    /**
+     * Assigns a position to the flag (ex. 0, 1, 2, 3, etc.)
+     *
+     * @param {*} position
+     */
+    setPosition(position) {
+        this.position = position;
+    }
+
+    addAndDisplayDomElement(domElement, draggedAway) {
+        const domFlag = new DOMFlag(domElement, this.id, draggedAway);
     }
 }
 
-const flagData = {
-    "femaleFlag": {
+class DOMFlag {
+    domElement;
+    flagInstanceId;
+    draggie;
+    id;
+    draggedAway;
+
+    constructor(domElement, flagInstanceId, draggedAway) {
+        this.domElement = domElement;
+        this.flagInstanceId = flagInstanceId;
+        this.id = currentFlagId;
+        this.draggedAway = draggedAway;
+        currentFlagId++;
+
+        this.addThisToDOMFlags();
+        this.addDraggie();
+        this.displayDiv();
+    }
+
+    addThisToDOMFlags() {
+        domFlags.push(this);
+    }
+
+    addDraggie() {
+        this.draggie = $(this.domElement).draggabilly({
+            containment: true,
+        }).on('dragStart', (event, pointer) => {
+            this.dragStartHandler(event, pointer, this);
+        }).on('dragEnd', (event, pointer) => {
+            this.dragEndHandler(event, pointer);
+        });
+
+        // set the position to absolute again because draggabilly changes it to relative
+        this.domElement.style.position = 'absolute';
+    }
+
+    getFlagInstance() {
+        return flags.filter(flag => flag.id === this.flagInstanceId)[0];
+    }
+
+    dragStartHandler(event, pointer, DOMFlagInstance) {
+        DOMFlagInstance.setDraggedAway(true);
+        const flagInstance = this.getFlagInstance();
+        createAndAppendDraggable(flagInstance, null);
+    }
+
+    dragEndHandler(event, pointer) {
+        const draggedDomFlagInstance = this;
+        const draggedFlagInstance = draggedDomFlagInstance.getFlagInstance();
+
+        const draggedAwayFlags = domFlags.filter(domFlagInstance => domFlagInstance.draggedAway === true && domFlagInstance.id !== draggedDomFlagInstance.id);
+        draggedAwayFlags.forEach(stillDomFlagInstance => {
+            const stillFlagInstance = stillDomFlagInstance.getFlagInstance();
+
+            // if (stillDomFlagInstance.id === draggedDomFlagInstance.id) {
+            //     console.log('Same flag');
+            //     return;
+            // }
+
+            console.log(stillDomFlagInstance.draggie.data('draggabilly').position);
+            const stillDomFlagPosition = stillDomFlagInstance.draggie.data('draggabilly').position;
+
+            const adjustedPositionX = gameboard.offsetLeft + stillDomFlagPosition.x;
+            const adjustedPositionY = gameboard.offsetTop + stillDomFlagPosition.y;
+
+            if (adjustedPositionX <= pointer.pageX && pointer.pageX <= adjustedPositionX + 100 &&
+                adjustedPositionY <= pointer.pageY && pointer.pageY <= adjustedPositionY + 100) {
+
+                const draggedFlagName = draggedFlagInstance.name;
+                const stillFlagName = stillFlagInstance.name;
+
+                flags.forEach((flag, i) => {
+                    if (flag.parents === null) {
+                        return;
+                    }
+
+                    if (flag.parents.a === draggedFlagName && flag.parents.b === stillFlagName ||
+                        flag.parents.a === stillFlagName && flag.parents.b === draggedFlagName) {
+
+                        const flagToActivate = flags[i];
+
+                        if (!flagToActivate.discovered) {
+                            const discoveredFlagsCount = flags.filter(flag => flag.discovered === true).length;
+
+                            flagToActivate.discovered = true;
+                            flagToActivate.setPosition(discoveredFlagsCount);
+
+                            createAndAppendDraggable(flagToActivate);
+                        }
+
+                        createAndAppendDraggable(flagToActivate, stillDomFlagPosition);
+
+
+                        // remove the merged flags from the dom
+                        draggedDomFlagInstance.domElement.remove();
+                        stillDomFlagInstance.domElement.remove();
+
+                        // remove the merged flags from the domFlags array
+                        domFlags = domFlags.filter(domFlag => domFlag !== draggedDomFlagInstance && domFlag !== stillDomFlagInstance);
+
+                    }
+                });
+            }
+        });
+    }
+
+    displayDiv() {
+        gameboard.appendChild(this.domElement);
+    }
+
+    setDraggedAway(draggedAway) {
+        this.draggedAway = draggedAway;
+    }
+}
+
+const flagData = [{
         name: 'Female Flag',
         backgroundColor: 'pink',
         position: 0,
         parents: null,
     },
-    "maleFlag": {
+    {
         name: 'Male Flag',
         backgroundColor: 'blue',
         position: 1,
         parents: null,
     },
-    "gayFlag": {
+    {
         position: 2,
         name: 'Gay Pride Flag',
         backgroundColor: 'red',
@@ -36,7 +168,7 @@ const flagData = {
             'b': 'Male Flag',
         },
     },
-    "testFlag": {
+    {
         position: 3,
         name: 'test flag',
         backgroundColor: 'green',
@@ -44,141 +176,60 @@ const flagData = {
             'a': 'Gay Pride Flag',
             'b': 'Male Flag',
         },
-
     },
-};
-
-let flags = {};
-
-for (const [key, value] of Object.entries(flagData)) {
-    flags[key] = new Flag(value.name, value.backgroundColor, value.position, value.parents);
-}
-
-let discoveredFlags = [
-    flags["femaleFlag"],
-    flags["maleFlag"],
 ];
 
-let draggedAwayFlags = [];
 
 /**
- * Creates and appends a draggable div element to the gameboard.
+ * Creates and appends a draggable div flag to the gameboard.
  *
- * @param {Object} element - The element object containing the properties of the draggable div.
+ * @param {Object} flag - The flag object containing the properties of the draggable div.
  * @param {number} i - The index of the draggable div.
  */
-function createAndAppendDraggable(element) {
+function createAndAppendDraggable(flag, position) {
+    let draggedAway = false;
     const draggableDiv = document.createElement('div');
     draggableDiv.classList.add('draggable');
-    draggableDiv.style.backgroundColor = element.backgroundColor;
+    draggableDiv.style.backgroundColor = flag.backgroundColor;
     draggableDiv.style.left = '10px';
-    draggableDiv.style.top = `${10 + element.position * 100 + 10 * element.position}px`;
-    draggableDiv.textContent = element.name;
+    draggableDiv.textContent = flag.name;
 
-    gameboard.appendChild(draggableDiv);
-    makeDraggableAndClone(draggableDiv, element);
+    if (position) {
+        draggableDiv.style.left = position.x + 'px';
+        draggableDiv.style.top = position.y + 'px';
+        draggedAway = true;
+    } else {
+        draggableDiv.style.top = `${10 + flag.position * 100 + flag.position * 10}px`;
+    }
+
+    flag.addAndDisplayDomElement(draggableDiv, draggedAway);
 }
 
-function createAndAppendDraggableWithPosition(element, position) {
+function createAndAppendDraggableWithPosition(flag, position) {
     const draggableDiv = document.createElement('div');
     draggableDiv.classList.add('draggable');
-    draggableDiv.style.backgroundColor = element.backgroundColor;
-    draggableDiv.style.left = position.x + 'px';
-    draggableDiv.style.top = position.y + 'px';
-    draggableDiv.textContent = element.name;
+    draggableDiv.style.backgroundColor = flag.backgroundColor;
+    draggableDiv.textContent = flag.name;
 
     gameboard.appendChild(draggableDiv);
-    makeDraggableAndClone(draggableDiv, element);
+    makeDraggableAndClone(draggableDiv, flag);
 }
 
-function makeDraggableAndClone(draggable, jsObject) {
-    // Initialize draggabilly and attach event handlers
-    $(draggable).draggabilly({
-        containment: true,
-    }).on('dragStart', function (event, pointer) {
-        // Bind this to dragStartHandler
-        dragStartHandler.call(this, event, pointer, jsObject);
-    }).on('dragEnd', dragEndHandler);
-
-    // Retrieve the draggabilly instance and add metadata
-    let draggy = $(draggable).data('draggabilly');
-    draggy.metadata = jsObject;
-}
-
-function dragStartHandler(event, pointer, jsObject) {
-    const draggie = $(this).data('draggabilly');
-    draggie.metadata.id = currentFlagId;
-    currentFlagId++;
-    draggedAwayFlags.push($(this));
-    const position = draggie.position;
-
-    const clone = this.cloneNode(true); // Clone the DOM element
-    clone.style.left = position.x + 'px';
-    clone.style.top = position.y + 'px';
-    clone.classList.remove('is-pointer-down', 'is-dragging');
-
-    const clonedObject = Object.assign({}, jsObject)
-
-    gameboard.appendChild(clone);
-    makeDraggableAndClone(clone, clonedObject);
-}
-
-function dragEndHandler(event, pointer) {
-    // Retrieve the draggabilly instance
-    const draggieX = $(this).data('draggabilly');
-    const nameParentX = draggieX.metadata.name;
 
 
-    draggedAwayFlags.forEach(flag => {
-        const draggieY = flag.data('draggabilly');
-        position = draggieY.position;
-        // check if the mouse cursor is on the flag
-        const flagId = draggieY.metadata.id;
-        const draggedFlagId = draggieX.metadata.id;
 
-
-        if (flagId === draggedFlagId) {
-            // Same ID
-            // This will have to be updated so that the id is comprised of the overarching object and the draggie instances
-            return;
-        }
-
-        const adjustedPositionX = gameboard.offsetLeft + position.x;
-        const adjustedPositionY = gameboard.offsetTop + position.y;
-
-        if (adjustedPositionX <= pointer.pageX && pointer.pageX <= adjustedPositionX + 100 &&
-            adjustedPositionY <= pointer.pageY && pointer.pageY <= adjustedPositionY + 100) {
-            // Mouse cursor is on the flag
-            const nameParentY = draggieY.metadata.name;
-
-
-            for (const [key, value] of Object.entries(flags)) {
-                if (value.parents === null) {
-                    continue;
-                }
-                if (value.parents.a === nameParentX && value.parents.b === nameParentY ||
-                    value.parents.a === nameParentY && value.parents.b === nameParentX) {
-                    const newFlag = flags[key];
-
-                    createAndAppendDraggableWithPosition(newFlag, {
-                        x: position.x,
-                        y: position.y
-                    });
-                    createAndAppendDraggable(newFlag);
-
-                    flag.remove();
-                    $(this).remove();
-                    // I need to remove the flag from the draggedAwayFlags array
-                    // The thing is it needs to be a the specific flag
-                    draggedAwayFlags.splice(draggedAwayFlags.indexOf(flag), 1);
-                    draggedAwayFlags.splice(draggedAwayFlags.indexOf(this), 1);
-                }
-            }
-        }
-    });
-}
-
-// Initial call for existing draggable elements
 $(document).ready(function () {
-    discoveredFlags.forEach(createAndAppendDraggable);
+
+    // load the flags array with the flag objects
+    for (const [i, flag] of flagData.entries()) {
+        flags[i] = new Flag(flag.name, flag.backgroundColor, flag.parents, i);
+    }
+
+    // add the first two flags to the discovered flags array and assign them a position
+    discoveredFlags = flags.filter(flag => flag.name === "Female Flag" || flag.name === "Male Flag");
+    discoveredFlags.forEach((flag, i) => {
+        flag.discovered = true;
+        flag.setPosition(i);
+    });
+    discoveredFlags.forEach(flag => createAndAppendDraggable(flag, null));
 });
