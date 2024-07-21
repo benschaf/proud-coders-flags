276bfa7badc3636e7e8908c839e1aa37a4d85f35
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-21 14:51:46 +0200

    feat: support both dialogs
    this could definitely be done more elegantly

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -1,24 +1,41 @@
 $(document).ready(function () {
-    // https://blog.webdevsimplified.com/2023-04/html-dialog/
-    const dialog = document.querySelector('dialog');
-    const instructionsModal = document.querySelector('#instructions');
-    $('#show-instructions').on("click", function () {
-        instructionsModal.showModal();
-    });
+  // https://blog.webdevsimplified.com/2023-04/html-dialog/
+  const flagInfo = document.querySelector('#flag-info');
+  const instructionsModal = document.querySelector('#instructions');
+  $('#show-instructions').on("click", function () {
+    instructionsModal.showModal();
+  });
 
-    dialog.addEventListener("click", e => {
-        const dialogDimensions = dialog.getBoundingClientRect()
-        if (
-          e.clientX < dialogDimensions.left ||
-          e.clientX > dialogDimensions.right ||
-          e.clientY < dialogDimensions.top ||
-          e.clientY > dialogDimensions.bottom
-        ) {
-          dialog.close()
-        }
-      });
+  $('#close-instructions').on("click", function () {
+    instructionsModal.close();
+  });
 
-    $('.dialog-close-button').click(function () {
-        dialog.close()
-    });
+  instructionsModal.addEventListener("click", e => {
+    const dialogDimensions = instructionsModal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      instructionsModal.close()
+    }
+  });
+
+
+  flagInfo.addEventListener("click", e => {
+    const dialogDimensions = flagInfo.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      flagInfo.close()
+    }
+  });
+
+  $('.dialog-close-button').click(function () {
+    flagInfo.close()
+  });
 });
