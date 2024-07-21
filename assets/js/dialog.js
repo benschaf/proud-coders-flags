419fdccfeeb995c6a6eacfa294876bdda74e5cd5
Benjamin Schäfer <benibschafer@gmail.com>
2024-07-21 16:05:20 +0200

    feat: add hints dialog and button

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -2,12 +2,21 @@ $(document).ready(function () {
   // https://blog.webdevsimplified.com/2023-04/html-dialog/
   const flagInfo = document.querySelector('#flag-info');
   const instructionsModal = document.querySelector('#instructions');
+  const hintsModal = document.querySelector('#hints-dialog');
+
   $('#show-instructions').on("click", function () {
     instructionsModal.showModal();
   });
 
   $('#close-instructions').on("click", function () {
     instructionsModal.close();
+  });
+
+  $('#show-hints').on("click", function () {
+    hintsModal.showModal();
+  });
+  $('#close-hints').on("click", function () {
+    hintsModal.close();
   });
 
   instructionsModal.addEventListener("click", e => {
@@ -19,6 +28,18 @@ $(document).ready(function () {
       e.clientY > dialogDimensions.bottom
     ) {
       instructionsModal.close()
+    }
+  });
+
+  hintsModal.addEventListener("click", e => {
+    const dialogDimensions = hintsModal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      hintsModal.close()
     }
   });
 
